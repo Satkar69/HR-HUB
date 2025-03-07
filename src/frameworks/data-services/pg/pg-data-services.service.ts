a7f8ad974fe2697ee4fe3b82861d9770fba7d52b
SatkarNiraula <satkarniraula69@gmail.com>
2025-03-07 13:57:12 +0545

    update :: add UserEntity

--- src/frameworks/data-services/pg/pg-data-services.service.ts
+++ src/frameworks/data-services/pg/pg-data-services.service.ts
@@ -8,14 +8,22 @@ import { AdminModel } from 'src/core/models';
 import { DataSource, Repository } from 'typeorm';
 import { AdminEntity } from './entities/admin.entity';
 import { PgAdminRepository } from './repositories/admin.repository';
+import { UserModel } from 'src/core/models/user.model';
+import { PgGenericRepository } from './pg-generic-repository';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class PgDataServices implements IDataServices, OnApplicationBootstrap {
   admin: IAdminRepository<AdminModel>;
+  user: PgGenericRepository<UserModel>;
 
   constructor(
     @Inject(AdminEntity.REPOSITORY)
     private adminRepository: Repository<AdminEntity>,
+
+    @Inject(UserEntity.REPOSITORY)
+    private userRepository: Repository<UserEntity>,
+
     private readonly cls: IClsStore<AppClsStore>,
     @Inject(InjectableString.APP_DATA_SOURCE)
     private dataSource: DataSource,
@@ -24,5 +32,6 @@ export class PgDataServices implements IDataServices, OnApplicationBootstrap {
   onApplicationBootstrap() {
     // admin
     this.admin = new PgAdminRepository(this.cls, this.adminRepository);
+    this.user = new PgGenericRepository(this.cls, this.userRepository);
   }
 }
