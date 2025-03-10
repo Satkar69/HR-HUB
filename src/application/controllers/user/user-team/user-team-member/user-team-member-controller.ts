09f824c05e079f52fd465506010cf8a373052f90
SatkarNiraula <satkarniraula69@gmail.com>
2025-03-10 18:37:34 +0545

    update :: added @Manager decorator to get team members

--- src/application/controllers/user/user-team/user-team-member/user-team-member-controller.ts
+++ src/application/controllers/user/user-team/user-team-member/user-team-member-controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { IPaginationQuery } from 'src/common/interface/response/interface/pagination.options.interface';
 import { CoreApiResponse } from 'src/application/api/core-api-response';
 import { UserTeamMemberUseCaseService } from 'src/use-cases/user-use-cases/user-team/user-team-member/user-team-member-use-case.service';
+import { Manager } from 'src/application/decorators/manager.decorator';
 
 @Controller('/team/member')
 export class UserTeamMemberController {
@@ -9,6 +10,7 @@ export class UserTeamMemberController {
     private userTeamMemberUseCaseService: UserTeamMemberUseCaseService,
   ) {}
 
+  @Manager()
   @Get('/get-my-members')
   async getMyTeamMembers(@Query() query: IPaginationQuery) {
     return CoreApiResponse.pagination(
