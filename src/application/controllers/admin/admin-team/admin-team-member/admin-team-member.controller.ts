16c290cde98a3954654d534c42083c029ef742c5
SatkarNiraula <satkarniraula69@gmail.com>
2025-03-11 10:51:20 +0545

    update :: admin add team member for a team api

--- src/application/controllers/admin/admin-team/admin-team-member/admin-team-member.controller.ts
+++ src/application/controllers/admin/admin-team/admin-team-member/admin-team-member.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AdminTeamMemberUseCaseService } from 'src/use-cases/admin-use-cases/admin-team/admin-team-member/admin-team-member-use-case.service';
 import { IPaginationQuery } from 'src/common/interface/response/interface/pagination.options.interface';
 import { CoreApiResponse } from 'src/application/api/core-api-response';
+import { CreateTeamMemberDto } from 'src/core/dtos/request/teamMember.dto';
 
 @Controller('/team')
 export class AdminTeamMemberController {
@@ -17,6 +18,15 @@ export class AdminTeamMemberController {
     return CoreApiResponse.pagination(
       await this.adminTeamMemberUseCaseService.getTeamMembersByTeam(teamId),
       query,
+    );
+  }
+
+  @Post('/member/create')
+  async createTeamMember(@Body() createTeamMemberDto: CreateTeamMemberDto) {
+    return CoreApiResponse.success(
+      await this.adminTeamMemberUseCaseService.addTeamMember(
+        createTeamMemberDto,
+      ),
     );
   }
 }
--- src/use-cases/admin-use-cases/admin-team/admin-team-member/admin-team-member-use-case.service.ts
+++ src/use-cases/admin-use-cases/admin-team/admin-team-member/admin-team-member-use-case.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { IPaginationData } from 'src/common/interface/response/interface/response-data.interface';
 import { IDataServices } from 'src/core/abstracts';
+import { AdminTeamMemberFactoryUseCaseService } from './admin-team-member-factory-use-case.service';
+import { CreateTeamMemberDto } from 'src/core/dtos/request/teamMember.dto';
+import { TeamMemberModel } from 'src/core/models/team-member.model';
 
 @Injectable()
 export class AdminTeamMemberUseCaseService {
-  constructor(private dataServices: IDataServices) {}
+  constructor(
+    private dataServices: IDataServices,
+    private adminTeamMemberFactoryUseCaseService: AdminTeamMemberFactoryUseCaseService,
+  ) {}
 
   async getTeamMembersByTeam(teamId: number): Promise<IPaginationData> {
     return await this.dataServices.teamMember.getAll({ team: { id: teamId } });
+  }
+
+  async addTeamMember(createTeamMemberDto: CreateTeamMemberDto) {
+    const newTeamMember =
+      this.adminTeamMemberFactoryUseCaseService.createTeamMember(
+        createTeamMemberDto,
+      );
+    return await this.dataServices.teamMember.create(newTeamMember);
+  }
+
+  async removeTeamMember(teamMemberId: number) {
+    const teamMember = await this.dataServices.teamMember.getOne({
+      id: teamMemberId,
+    });
+    return await this.dataServices.teamMember.remove(teamMember);
   }
 }
