4fc242fcd4306628ec0d34047a092a3a662d8f13
SatkarNiraula <satkarniraula69@gmail.com>
2025-03-16 14:21:31 +0545

    update :: update questionnaires by review api and services

--- src/application/controllers/user/user-review/user-review-questionnaire/user-review-questionnaire.controller.ts
+++ src/application/controllers/user/user-review/user-review-questionnaire/user-review-questionnaire.controller.ts
@@ -1,20 +1,33 @@
-import { Body, Controller, Patch } from '@nestjs/common';
+import { Body, Controller, Param, Patch } from '@nestjs/common';
 import { CoreApiResponse } from 'src/application/api/core-api-response';
 import { UpdateQuestionnairesDto } from 'src/core/dtos/request/questionnaire.dto';
 import { UserReviewQuestionnaireUseCaseService } from 'src/use-cases/user-use-cases/user-review/user-review-questionnaire/user-review-questionnaire-use-case.service';
 
-@Controller('/review/questionnaire')
+@Controller('/review')
 export class UserReviewQuestionnaireController {
   constructor(
     private userReviewQuestionnaireUseCaseService: UserReviewQuestionnaireUseCaseService,
   ) {}
 
-  @Patch('/update')
-  async updateReviewQuestionnaires(
+  // @Patch('/questionnaire/update')
+  // async updateReviewQuestionnaires(
+  //   @Body() updateQuestionnairesDto: UpdateQuestionnairesDto,
+  // ) {
+  //   return CoreApiResponse.success(
+  //     await this.userReviewQuestionnaireUseCaseService.updateQuestionnaires(
+  //       updateQuestionnairesDto,
+  //     ),
+  //   );
+  // }
+
+  @Patch('/:id/questionnaire/update')
+  async updateQuestionnaireByReview(
+    @Param('id') reviewId: number,
     @Body() updateQuestionnairesDto: UpdateQuestionnairesDto,
   ) {
     return CoreApiResponse.success(
-      await this.userReviewQuestionnaireUseCaseService.updateQuestionnaires(
+      await this.userReviewQuestionnaireUseCaseService.updateQuestionnairesByReviewId(
+        reviewId,
         updateQuestionnairesDto,
       ),
     );
