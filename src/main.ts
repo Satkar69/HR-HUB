fa42f96cd39d1936fc13145a200c7a9e801c6206
SatkarNiraula <satkarniraula69@gmail.com>
2025-03-16 21:08:50 +0545

    update :: add frontend url in cors origin

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,12 @@ async function bootstrap() {
 
   // -- Cors setup
   app.enableCors({
-    origin: ['*', 'http://localhost:3000', 'http://localhost:3001'],
+    origin: [
+      '*',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://hr-portal-teal.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
