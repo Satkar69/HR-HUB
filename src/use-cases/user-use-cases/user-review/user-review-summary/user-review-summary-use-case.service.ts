1dafc4289fec88128f47730c4ea7b98f3e06ed17
SatkarNiraula <satkarniraula69@gmail.com>
2025-03-20 22:17:19 +0545

    add a TODO comment for user review summary use case service

--- src/use-cases/user-use-cases/user-review/user-review-summary/user-review-summary-use-case.service.ts
+++ src/use-cases/user-use-cases/user-review/user-review-summary/user-review-summary-use-case.service.ts
@@ -4,6 +4,7 @@ import { AppClsStore } from 'src/common/interface/app-cls-store.interface';
 import { UserClsData } from 'src/common/interface/app-cls-store.interface';
 import { IClsStore } from 'src/core/abstracts/adapters/cls-store.abstract';
 
+// TODO :: make the necessary services to get the user's review summary and to get user's acknowledgement
 @Injectable()
 export class UserReviewSummaryUseCaseService {
   constructor(
